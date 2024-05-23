f0a17a7ef7fbc29e6da0833e5836766a578108a6
quocan <quocan050302@gmail.com>
2024-05-23 17:46:04 +0700

    fix bug

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,16 +1,16 @@
 "use client";
-import WorkList from "./components/workList"
+import WorkList from "./components/WorkList";
 import HeaderLogo from "@/components/headerLogo/HeaderLogo";
-import "./work.scss"
+import "./work.scss";
 function Work() {
-    return (
-		<main>	
-			<HeaderLogo />
-			<div className="work is-visible">
-				<WorkList />
-			</div>
-		</main>
-    )
+  return (
+    <main>
+      <HeaderLogo />
+      <div className="work is-visible">
+        <WorkList />
+      </div>
+    </main>
+  );
 }
 
-export default Work
+export default Work;
